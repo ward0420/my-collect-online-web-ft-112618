e7f09426f6cb30df4038bd0e89e932b7c6b73b6b
Don Ward <10is0420@gmail.com>
2018-12-12 23:15:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,27 @@
+def my_collect(array)
+  
+  languages = []
+  
+  array.collect do |lang|
+    
+    languages << yield(lang.upcase) 
+    
+  end
 
+  languages
 
+end
+
+def my_collect(students)
+  
+  language = []
+  
+  array.collect do |lang|
+    
+    languages << yield(lang.upcase) 
+    
+  end
+
+  languages
+
+end
